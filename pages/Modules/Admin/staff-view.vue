a38333a9457d6a9fb0cd820360f1b0b664e8fa55
f41zf4u21 <f41zf4u21@gmail.com>
2023-08-25 12:49:46 +0800

    pentadbir

--- pages/Modules/Admin/staff-view.vue
+++ pages/Modules/Admin/staff-view.vue
@@ -6,19 +6,11 @@
       <main>
         <div class="container-fluid px-4">
           <div class="page-title">
-            <h1>Staff Management</h1>
+            <h1>Pengurusan Pentadbiran</h1>
           </div>
           <div class="card mb-4">
             <div class="card-header">
-              <h4>Mentari Staff Detail</h4>
-              <div class="icon-set">
-                <a  @click="gotoedit" title="edit record"
-                  ><i class="fa fa-edit"></i
-                ></a>
-                <a @click="gototransfer" title="transfer staff"
-                  ><i class="fa fa-share-square"></i
-                ></a>
-              </div>
+              <h4>Butiran Pentadbir</h4>
             </div>
             <div class="card-body">
               <table
@@ -34,81 +26,27 @@
                 </thead>
                 <tbody>
                   <tr>
-                    <td>Name</td>
+                    <td>Nama</td>
                     <td>{{ staffdetail.name }}</td>
                   </tr>
-
                   <tr>
-                    <td>NRIC NO</td>
-                    <td>{{ staffdetail.nric_no }}</td>
-                  </tr>
-
-                  <tr>
-                    <td>Designation</td>
-                    <td>{{ staffdetail.designation_name }}</td>
-                  </tr>
-
-                  <tr>
-                    <td>Designation Period(Start Date)</td>
-                    <td>{{ staffdetail.designation_period_start_date }}</td>
-                  </tr>
-
-                  <tr>
-                    <td>Designation Period(End Date)</td>
-                    <td>{{ staffdetail.designation_period_end_date }}</td>
-                  </tr>
-
-                  <tr>
-                    <td>Profession Registration NO.</td>
-                    <td>{{ staffdetail.registration_no }}</td>
-                  </tr>
-
-                  <tr>
-                    <td>Role</td>
-                    <td>{{ staffdetail.role_name }}</td>
-                  </tr>
-
-                  <tr>
-                    <td>Team</td>
-                    <td>{{ staffdetail.service_name }}</td>
-                  </tr>
-
-                  <tr>
-                    <td>Location</td>
-                    <td>{{ staffdetail.hospital_branch_name }}</td>
-                  </tr>
-
-                  <tr>
-                    <td>Person In Charge</td>
-                    <td v-if="staffdetail.is_incharge==1">Yes</td>
-                    <td v-if="staffdetail.is_incharge==0">No</td>
-                  </tr>
-
-                  <tr>
-                    <td>Contact No</td>
-                    <td>{{ staffdetail.contact_no }}</td>
-                  </tr>
-
-                  <tr>
-                    <td>Email Address</td>
+                    <td>Alamat Emel</td>
                     <td>{{ staffdetail.email }}</td>
                   </tr>
-
                   <tr>
-                    <td>Account Active</td>
+                    <td>Peranan</td>
+                    <td>{{ staffdetail.role }}</td>
+                  </tr>
+                  <tr>
+                    <td>ID Pengguna</td>
+                    <td>{{ staffdetail.id_user }}</td>
+                  </tr>
+                  <tr>
+                    <td>Status Akaun</td>
                     <td v-if="staffdetail.status==1">Active</td>
                     <td v-if="staffdetail.status==0">Non-active account</td>
                   </tr>
 
-                  <tr>
-                    <td>Start Date</td>
-                    <td>{{ staffdetail.start_date }}</td>
-                  </tr>
-
-                  <tr>
-                    <td>End Date</td>
-                    <td>{{ staffdetail.end_date }}</td>
-                  </tr>
                 </tbody>
               </table>
 
